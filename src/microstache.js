edb0ac20e05c548b0a54de45473fcb703d206c15
RationalCoding <thomasmullendesign@gmail.com>
2017-02-20 18:41:16 -0500

    standard

--- src/microstache.js
+++ src/microstache.js
@@ -1,66 +1,47 @@
-/* Microstache minimal templating module by RationalCoding */
+window.Microstache = (function (my) {
+  function reEscape (str) {
+    return str.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&')
+  }
+  function deepFind (obj, path) {
+    var paths = path.split('.')
+    var current = obj
+    var i
 
-/*
-
-Takes an HTML string with mustache notation in it:
-
-"<h1>{{ title }}</h1>"
-
-and a data object:
-
-{
-    title : "My Title!"
-}
-
-and returns the templated result:
-
-"<h1>My Title!</h1>"
-
-*/
-Microstache = (function (my) {
-        function reEscape(str) {
-            return str.replace(/[\-\[\]\/\{\}\(\)\*\+\?\.\\\^\$\|]/g, "\\$&");
+    for (i = 0; i < paths.length; ++i) {
+      if (current[paths[i]] === undefined) {
+        return undefined
+      } else {
+        current = current[paths[i]]
+      }
+    }
+    return current
+  }
+  my.template = function (htmlTemplate, data) {
+    var re
+    var rendered
+    var key
+    var keys = []
+    function recurPush (obj, past) {
+      for (key in obj) {
+        if (typeof obj[key] === 'object') {
+          recurPush(obj[key], past + key + '.')
         }
-        function deepFind(obj, path) {
-            var paths = path.split('.'),
-                current = obj,
-                i;
 
-            for (i = 0; i < paths.length; ++i) {
-                if (current[paths[i]] == undefined) {
-                    return undefined;
-                } else {
-                    current = current[paths[i]];
-                }
-            }
-            return current;
-        }
-        my.template = function (htmlTemplate, data) {
-            var re,
-                rendered,
-                key,
-                keys = [];
-            function recurPush(obj, past){
-                for (key in obj){
-                    if (typeof obj[key] === 'object'){
-                        recurPush(obj[key], past+key+'.');
-                    }
-                    
-                    keys.push("{{\\s*" + reEscape((past+key).toString()) + "\\s*}}");
-                };
-            }  
-            recurPush(data, '');
-            keys = keys.join("|");
-            if (keys) {
-                re = new RegExp(keys, "gi");
-                rendered = htmlTemplate.replace(re, function (m) {
-                    var re = /{{\s*([^\s]*)\s*}}/g;
-                    return deepFind(data,re.exec(m.toString())[1]);
-                });
-            } else {
-                rendered = htmlTemplate;
-            }
-            return rendered.replace(/{{[^}}]*}}/gi, "");
-        }
-        return my;
-    }({}))
+        keys.push('{{\\s*' + reEscape((past + key).toString()) + '\\s*}}')
+      };
+    }
+    recurPush(data, '')
+    keys = keys.join('|')
+    if (keys) {
+      re = new RegExp(keys, 'gi')
+      rendered = htmlTemplate.replace(re, function (m) {
+        var re = /{{\s*([^\s]*)\s*}}/g
+        return deepFind(data, re.exec(m.toString())[1])
+      })
+    } else {
+      rendered = htmlTemplate
+    }
+    return rendered.replace(/{{[^}}]*}}/gi, '')
+  }
+  return my
+}({}))
